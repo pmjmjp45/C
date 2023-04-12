18d807b9bd9675445c1120c851bb786e0854f04e
pmjmjp45 <pmjmjp45@gmail.com>
2023-04-12 12:31:59 +0900

    shorter version

--- Age.cpp
+++ Age.cpp
@@ -22,37 +22,18 @@ int main()
 	birthYear = number;
 	
 	if(gender == 1 || gender == 2) { // born in 19xx 
-		if (birthMonth < t->tm_mon + 1) {
-			age = t->tm_year - birthYear;
-		} else if (birthMonth == t->tm_mon + 1) {
-			if (birthDay <= t->tm_mday) {
-				age = t->tm_year - birthYear;
-			} else {
-				age = t->tm_year - birthYear - 1;
-			}
-		} else {
-			age = t->tm_year - birthYear - 1;
-		}
-		birthYear += 1900; 
-		
+		birthYear += 1900;
 	} else if (gender == 3 || gender == 4) { // born in 20xx
-		t->tm_year -= 100; // submit 100 from the this year 
-		
-		if (birthMonth < t->tm_mon + 1) {
-			age = t->tm_year - birthYear;
-		} else if (birthMonth == t->tm_mon + 1) {
-			if (birthDay <= t->tm_mday) {
-				age = t->tm_year - birthYear;
-			} else {
-				age = t->tm_year - birthYear - 1;
-			}
-		} else {
-			age = t->tm_year - birthYear - 1;
-		}
-		birthYear += 2000; 
+		birthYear += 2000;
 	} else {
 		printf("error! enter correctly");
 	}	
+	
+	age = t->tm_year + 1900 - birthYear;
+    
+	if (birthMonth > t->tm_mon + 1 || (birthMonth == t->tm_mon + 1 && birthDay > t->tm_mday)) {
+        age--; // before the birthday, age--
+    }
 	printf("Birthday: %d-%d-%d\n", birthYear,birthMonth,birthDay);	
 	printf("age: %d\n", age);	
 }
